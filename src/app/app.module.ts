26f5cb15e755e3567253cd6f044df27cb0c6d2b1
imhemantchaubey <codewithhemant@gmail.com>
2023-02-02 14:58:38 +0530

    updated the about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,10 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
 
-const appRoutes: Routes = [{ path: '', component: TasksComponent }];
+const appRoutes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
